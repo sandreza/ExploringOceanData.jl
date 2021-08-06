f51d75d54fe2601350868684575d63a86ee81ad9
Documenter.jl <documenter@juliadocs.github.io>
2021-08-06 00:03:03 +0000

    build based on 6b801b6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ExploringOceanData.jl","page":"Home","title":"ExploringOceanData.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ExploringOceanData.jl","category":"page"}]
+}
